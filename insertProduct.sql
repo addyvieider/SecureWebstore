7b9b5dcf54a73b6ed008582378e9858a3c97753a
Rosace Angelo (Student Com15) <angelo.rosace@stud-inf.unibz.it>
2018-06-06 18:06:29 +0200

    Upload New File

---
+++ insertProduct.sql
@@ -0,0 +1,120 @@
+#1
+INSERT INTO product VALUES ("damm_estrella","Damm Estrella",NULL,"BESONDERHEITEN: 
+	Sehr klare, helle Farbe. Einstieg süßlich und malzig, sehr angenehm. 
+	Abgang hopfig und leicht bitter. Leicht wässrig, 
+	aber dennoch eines der besten spanischen Biere - im Sommer angenehm zu trinken.",
+	"GESCHMACK: hopfig und leicht bitter",
+	"a.png")
+INSERT INTO product_package VALUES ("damm_estrella","Single Bottle","1,50")
+INSERT INTO product_package VALUES ("damm_estrella","Sixpack","9")
+INSERT INTO product_package VALUES ("damm_estrella","Full Crate","36")
+#2
+INSERT INTO product VALUES ("san_miguel","San Miguel",NULL,"BESONDERHEITEN: Ein recht süffiges Bier. 
+	Im Einstieg schön mild, sanft und spritzig. 
+	Im Abgang wird es ein wenig herber und bitterer was aber gut passt.",
+	"GESCHMACK: sanft und spritzig",
+	"San_miguel.png")
+INSERT INTO product_package VALUES ("san_miguel","Single Bottle","1,50")
+INSERT INTO product_package VALUES ("san_miguel","Sixpack","9")
+INSERT INTO product_package VALUES ("san_miguel","Full Crate","36")
+#3
+INSERT INTO product VALUES ("tennent's_super","Tennent's Super",NULL,"BESONDERHEITEN: Das Tennents hat eine helle blanke Bernstein Farbe mit wenig gemischtporigem Schaum. 
+	Malzig, süßlich und alkoholisch im Geruch. Der Geschmack ist malzig, süßlich aber auch etwas künstlich zuckerig. 
+	Positiv ist, dass man den Alkohol sehr wenig rausschmeckt. Der ist gut eingebunden. 
+	Im Abgang sogar leicht hopfig. Ein Bier zum Wegschädeln.",
+	"GESCHMACK: malzig und süßlich",
+	"Tennet's_super.png")
+INSERT INTO product_package VALUES ("tennent's_super","Single Bottle","1,6")
+INSERT INTO product_package VALUES ("tennent's_super","Sixpack","9,6")
+INSERT INTO product_package VALUES ("tennent's_super","Full Crate","38.4")
+#4
+INSERT INTO product VALUES ("budweiser","Budweiser",NULL,"BESONDERHEITEN: Bud beer is dull yellow in colour, opalescent and almost transparent with a reduced perlage and a scarce fizziness.
+These few bubbles are a plus for Bud beer, which is consumed directly from the bottle, otherwise it would swell the stomach.
+The foam is white and compact.
+The nose reveals a hint of hops and an alcoholic tip.",
+	"GESCHMACK: mild and fresh",
+	"budweiser.png")
+INSERT INTO product_package VALUES ("budweiser","Single Bottle","1,5")
+INSERT INTO product_package VALUES ("budweiser","Sixpack","9")
+INSERT INTO product_package VALUES ("budweiser","Full Crate","36")
+#5
+INSERT INTO product VALUES ("heineken","Heineken",NULL,"BESONDERHEITEN: The Heineken, with its international profile, is one of the most famous Lagers in the world.
+A beer that we can find in every corner of the world and in every shape, on tap, in bottle and in can. A blonde with a little persistent foam and soft aromas, almost nil.
+The body is flat, in the mouth we feel very sparkling and almost pasty, the taste is dominated by bitterness.",
+	"GESCHMACK: mildly bitter",
+	"heineken.png")
+INSERT INTO product_package VALUES ("heineken","Single Bottle","2")
+INSERT INTO product_package VALUES ("heineken","Sixpack","12")
+INSERT INTO product_package VALUES ("heineken","Full Crate","48")
+#6
+INSERT INTO product VALUES ("moretti","Moretti",NULL,"BESONDERHEITEN: When traditions meet the quality of the ingredients, the result is always extraordinary. Our master brewers have created Birra Moretti La Bianca in the Weiss style, but using 100% Italian malts: 50% barley malt and 50% wheat malt. The result is a light and naturally cloudy beer with a fresh aroma, rich in spicy and floral notes.",
+	"GESCHMACK: delicate and refreshing with an elegant and balanced aftertaste",
+	"moretti.png")
+INSERT INTO product_package VALUES ("moretti","Single Bottle","2")
+INSERT INTO product_package VALUES ("moretti","Sixpack","12")
+INSERT INTO product_package VALUES ("moretti","Full Crate","48")
+#7
+INSERT INTO product VALUES ("franziskaner_weissbier","Franziskaner Weissbier",NULL,"BESONDERHEITEN: Franziskaner belongs to the category of Hefeweizen, the pure version of weiss, characterised by a cloudy colour due to the presence of yeast which has remained in suspension, i.e. unfiltered, whose presence contributes to giving the typical sweetish flavour.",
+	"GESCHMACK: sweet",
+	"franziskaner_weissbier.png")
+INSERT INTO product_package VALUES ("franziskaner_weissbier","Single Bottle","2")
+INSERT INTO product_package VALUES ("franziskaner_weissbier","Sixpack","12")
+INSERT INTO product_package VALUES ("franziskaner_weissbier","Full Crate","48")
+#8
+INSERT INTO product VALUES ("dreher","Dreher",NULL,"BESONDERHEITEN: Dreher Beer is a Lager beer, so low-fermentation beer, with an alcohol content of 4.7%. It has a straw yellow color with golden reflections, a fine foam, compact and tight.",
+	"GESCHMACK: The aromas are delicate and traceable to the cereal, with a moderately hopped taste with hints of honey.",
+	"dreher.png")
+INSERT INTO product_package VALUES ("dreher","Single Bottle","1,5")
+INSERT INTO product_package VALUES ("dreher","Sixpack","9")
+INSERT INTO product_package VALUES ("dreher","Full Crate","36")
+#9
+INSERT INTO product VALUES ("ichnusa","Ichnusa",NULL,"BESONDERHEITEN: It shows a straw yellow color with golden reflections, the foam is white, fine and persistent. The nose reveals intense aromas of hops, cereals, white flowers and citrus fruits. On the palate it is fresh, has a good balance between the sweet notes and the more bitter ones and leaves a good aftertaste persistence.",
+	"GESCHMACK: good balance between the sweet notes and the more bitter ones and leaves a good aftertaste persistence.",
+	"ichnusa.png")
+INSERT INTO product_package VALUES ("ichnusa","Single Bottle","1,6")
+INSERT INTO product_package VALUES ("ichnusa","Sixpack","9,6")
+INSERT INTO product_package VALUES ("ichnusa","Full Crate","38.4")
+#10
+INSERT INTO product VALUES ("nastro_azzurro","Nastro Azzurro",NULL,"BESONDERHEITEN: Premium Italian beer has always been produced only with the finest varieties of barley marts and the finest quality hops. It is characterized by its strong, clean taste and transparency, the result of an exclusive production process that guarantees the freshness and naturalness of freshly tapped beer.",
+	"GESCHMACK: strong and clean",
+	"nastro_azzurro.png")
+INSERT INTO product_package VALUES ("nastro_azzurro","Single Bottle","2")
+INSERT INTO product_package VALUES ("nastro_azzurro","Sixpack","12")
+INSERT INTO product_package VALUES ("nastro_azzurro","Full Crate","48")
+#11
+INSERT INTO product VALUES ("peroni","Peroni",NULL,"BESONDERHEITEN: Peroni Classico is a pale golden yellow lager beer, low fermentation, moderately bitter taste, balanced hops and malt. The alcohol level is 4.7% vol. and should be served at a temperature of 4/6°C.",
+	"GESCHMACK: moderately bitter taste, balanced hops and malt",
+	"peroni.png")
+INSERT INTO product_package VALUES ("peroni","Single Bottle","1,6")
+INSERT INTO product_package VALUES ("peroni","Sixpack","9,6")
+INSERT INTO product_package VALUES ("peroni","Full Crate","38.4")
+#12
+INSERT INTO product VALUES ("poretti_chiara","Poretti chiara",NULL,"BESONDERHEITEN: A beverage beer with a fresh and malty flavor perfect for daily meals.
+Alcoholic Grade: 4.8%",
+	"GESCHMACK: fresh and malty flavor",
+	"poretti_chiara.png")
+INSERT INTO product_package VALUES ("poretti_chiara","Single Bottle","2")
+INSERT INTO product_package VALUES ("poretti_chiara","Sixpack","12")
+INSERT INTO product_package VALUES ("poretti_chiara","Full Crate","48")
+#13
+INSERT INTO product VALUES ("carlsberg","Carlsberg",NULL,"BESONDERHEITEN: Carlsberg belongs to the Lager segment. It is made of only malt and is a beer with low fermentation and light color.
+Its organoleptic characteristics allow it to stand out as an elegant and refined beer, while the careful selection of hops that compose it enhances the unique and unmistakable fragrance.",
+	"GESCHMACK: light",
+	"carlsberg.png")
+INSERT INTO product_package VALUES ("carlsberg","Single Bottle","1,6")
+INSERT INTO product_package VALUES ("carlsberg","Sixpack","9,6")
+INSERT INTO product_package VALUES ("carlsberg","Full Crate","38.4")
+#14
+INSERT INTO product VALUES ("corona","Corona",NULL,"BESONDERHEITEN: The Corona has an alcohol content of 4,5 % by volume and a straw yellow colour with a thin and evanescent foam. It is a beer with a light body with light hoped and fruity notes and a slight herbaceous aftertaste, to be consumed at a temperature of 5-7 degrees",
+	"GESCHMACK: slight hints of hops and fruit and a slight herbaceous aftertaste",
+	"corona.png")
+INSERT INTO product_package VALUES ("corona","Single Bottle","2")
+INSERT INTO product_package VALUES ("corona","Sixpack","12")
+INSERT INTO product_package VALUES ("corona","Full Crate","48")
+#15
+INSERT INTO product VALUES ("beck's","Beck's",NULL,"BESONDERHEITEN: Beck's shows a straw yellow color and a moderately persistent fine and abundant foam. The nose has hints of malt and fresh, pungent aromas of hops, reminiscent of freshly cut grass. The finish is a discreet body that is not particularly demanding and luptured.",
+	"GESCHMACK:  discreet body that is not particularly demanding and luptured.",
+	"beck's.png")
+INSERT INTO product_package VALUES ("beck's","Single Bottle","1,6")
+INSERT INTO product_package VALUES ("beck's","Sixpack","9,6")
+INSERT INTO product_package VALUES ("beck's","Full Crate","38.4")
